9e00fdf841e291594d17da410eaa531cacdc353c
Tim Addai <timodie.addai@gmail.com>
2022-02-23 18:44:00 -0500

    Create sample.js

--- sample.js
+++ sample.js
@@ -1,3 +1,7 @@
+"use strict";
+function myFunction() {
+  alert(this); // will alert "undefined"
+}
 function entryPoint() {
   console.log("you have loaded this in web app");
   console.log(document.cookie)
@@ -5,3 +9,5 @@ function entryPoint() {
 }
 
 window.__entry = entryPoint;
+
+entryPoint
